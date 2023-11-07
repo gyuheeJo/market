96f346720841dfaeeb56fd45fa5c8547b3ee98a5
gyuhee <davidgyuhee@gmail.com>
2023-11-06 21:57:27 -0500

    deleted SwaggerConfig, unnecessary

--- src/main/java/com/example/market/web/config/SwaggerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.market.web.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-
-public class SwaggerConfig {
-
-}
